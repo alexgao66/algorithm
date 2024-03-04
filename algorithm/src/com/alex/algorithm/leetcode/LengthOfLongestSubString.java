c661fb7b07850b602a70df5c3bd1b4320f8691ab
gaojun@meituan.com <gaojun@meituan.com>
2024-03-04 22:07:59 +0800

    leetCode 135 分糖 leetCode 3 最长无重复子串的长度

--- algorithm/src/com/alex/algorithm/leetcode/LengthOfLongestSubString.java
+++ algorithm/src/com/alex/algorithm/leetcode/LengthOfLongestSubString.java
@@ -4,10 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * leetCode 3题：求个最大无重复子串的长度
+ * 思路：使用滑动窗口
+ * 1）扩大窗口：从左至右依次遍历每个字符，并字符加入到map中（key为字符，value为出现的次数），移动窗口右指针；
+ * 2）缩小窗口：通过map判断该字符在窗口中是否出现小于或等于1次，否则移动窗口左指针。
+ *
  * @author: gaojun
  * @date: 2024/3/4
- * Copyright (C) 2022 Meituan
- * All rights reserved
  **/
 public class LengthOfLongestSubString {
 
@@ -16,25 +19,19 @@ public class LengthOfLongestSubString {
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> window = new HashMap<>();
-
-        int left = 0, right = 0;
-        int res = 0; // 记录结果
+        int left = 0, right = 0, maxLength = 0;
+        Map<Character, Integer> charCountMap = new HashMap<>();
         while (right < s.length()) {
-            char c = s.charAt(right);
-            right++;
-            // 进行窗口内数据的一系列更新
-            window.put(c, window.getOrDefault(c, 0) + 1);
-            // 判断左侧窗口是否要收缩
-            while (window.get(c) > 1) {
-                char d = s.charAt(left);
-                left++;
-                // 进行窗口内数据的一系列更新
-                window.put(d, window.get(d) - 1);
+            Character c1 = s.charAt(right);
+            ++right;
+            charCountMap.put(c1, charCountMap.getOrDefault(c1, 0) + 1);
+            while (charCountMap.get(c1) > 1) {
+                Character c2 = s.charAt(left);
+                ++left;
+                charCountMap.put(c2, charCountMap.get(c2) - 1);
             }
-            // 在这里更新答案
-            res = Math.max(res, right - left);
+            maxLength = Math.max(maxLength, right - left);
         }
-        return res;
+        return maxLength;
     }
 }
