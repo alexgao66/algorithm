8fd7480dc53e0a05f653d4703b7ec7606bfa3cd9
gaojun@meituan.com <gaojun@meituan.com>
2024-03-07 11:57:30 +0800

    bubble sort without improve

---
+++ algorithm/src/com/alex/algorithm/sort/BubbleSort.java
@@ -0,0 +1,40 @@
+package com.alex.algorithm.sort;
+
+/**
+ * 冒泡排序
+ * 排序逻辑：
+ * 两个相邻的元素
+ *
+ * @author: gaojun
+ * @date: 2024/3/5
+ **/
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        bubbleSort(bubbleSort(new int[]{4,1,3,1,5,2}));
+    }
+
+    public static int[] bubbleSort(int[] array) {
+        for(int i = array.length - 1; i > 0; --i) {
+            for (int j = 0; j < i; ++j) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+            printIntArray(array);
+            System.out.println();
+        }
+        return array;
+    }
+
+    private static void printIntArray(int[] array) {
+        for (int i = 0; i < array.length; ++i) {
+            System.out.print(array[i]);
+            if (i != array.length - 1) {
+                System.out.print(",");
+            }
+        }
+    }
+}
