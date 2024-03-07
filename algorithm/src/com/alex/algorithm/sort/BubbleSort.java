7590d20126ed9d8d9f929813308b0f88ead60b94
gaojun@meituan.com <gaojun@meituan.com>
2024-03-07 14:37:22 +0800

    bubble sort with improve

--- algorithm/src/com/alex/algorithm/sort/BubbleSort.java
+++ algorithm/src/com/alex/algorithm/sort/BubbleSort.java
@@ -11,7 +11,7 @@ package com.alex.algorithm.sort;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        bubbleSort(bubbleSort(new int[]{4,1,3,1,5,2}));
+        bubbleSort(new int[]{4,1,3,1,5,2});
     }
 
     public static int[] bubbleSort(int[] array) {
@@ -36,5 +36,25 @@ public class BubbleSort {
                 System.out.print(",");
             }
         }
+    }
+
+    public static int[] bubbleSortImprove(int[] array) {
+        for(int i = array.length - 1; i > 0; --i) {
+            boolean swap = false;
+            for (int j = 0; j < i; ++j) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                    swap = true;
+                }
+            }
+            if (!swap) {
+                break;
+            }
+            printIntArray(array);
+            System.out.println();
+        }
+        return array;
     }
 }
