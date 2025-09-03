94225bb9f3cdc7d1be273b90d324d2a6eb18533b
Alex.Gao <gaojun@meituan.com>
2025-09-03 08:18:35 +0800

    leetcod hot100 ThreeSum

---
+++ algorithm/src/com/alex/algorithm/leetcode/ThreeSum.java
@@ -0,0 +1,48 @@
+package com.alex.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 找出所有三个数字相加为0的组合，leetcode 15
+ * @author: gaojun
+ * @date: 2025/8/7
+ **/
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l + 1]){
+                        ++l;
+                    }
+                    while (l < r && nums[r] == nums[r-1]) {
+                        --r;
+                    }
+                    ++l;
+                    --r;
+                }else if(sum < 0) {
+                    ++l;
+                }else {
+                    --r;
+                }
+            }
+        }
+        return result;
+    }
+}
